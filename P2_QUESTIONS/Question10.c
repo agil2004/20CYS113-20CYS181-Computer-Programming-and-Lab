fd8d4109e8d5759abfd23816659bf9f312320d78
agil2004 <116860187+agil2004@users.noreply.github.com>
2023-06-22 15:47:12 +0530

    Add files via upload

---
+++ P2_QUESTIONS/Question10.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+struct complex {
+	int realpart;
+	int imgpart;
+
+	Struct multiply {
+		int realpart[20];
+		int imgpart[20];
+	}cm;
+   int main(){
+	   struct complex{
+           struct multiply {
+		   scanf ("%d",&cm.realpart);
+		   scanf("%d",&cm.imgpart);
+		   printf ("%d \n",cm.realpart*cm.imgpart);
+		   printf ("%d \n",cm.realpart*cm.imgpart);
+
+		   return 0;
+	   }
---
+++ P2_QUESTIONS/Question2.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    int i,j,rows;
+    for (int i=0;i<=8;i++){
+        printf ("%d \n",i);
+    }
+    for (int j=0;j<=8;j++){
+        printf ("%d \n",j);
+    }
+    for (int i=0;i<=8;i++){
+        printf ("%d \n",i+1);
+    }
+    for (int j=0;j<=8;j++){
+        printf ("%d \n",j+1);
+    }
+    return 0;
+}
